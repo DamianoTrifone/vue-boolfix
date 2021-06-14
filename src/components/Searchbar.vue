56224a5e2195d1f78e4d82077db1c67065438932
DamianoTrifone <trifone.damiano@gmail.com>
2021-06-14 12:31:39 +0200

    Campo di ricerca

---
+++ src/components/Searchbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="searchbar">
+      <div class="ricerca">
+        <input type="text">
+        <button>Cerca</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Searchbar"
+}
+</script>
+
+<style lang="scss">
+    .searchbar{
+        height: 80px;
+        background-color: black;
+    }
+
+    .ricerca{
+        display: flex;
+        justify-content: flex-end;
+        align-self: flex-end;
+        }
+
+    .ricerca input{
+        margin-top: 40px;
+        margin-right: 20px;
+    }
+
+    .ricerca button{
+        margin-top: 40px;
+        margin-right: 20px;
+    }
+</style>
