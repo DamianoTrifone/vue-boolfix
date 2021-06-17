dc17b76a1fff34de5352abe8b692d9bceaf7e870
DamianoTrifone <trifone.damiano@gmail.com>
2021-06-17 10:49:09 +0200

    Stelline voto

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -2,11 +2,23 @@
 <main>
   <div class="film">
       <li>
-        <img class="copertina" :src="'https://image.tmdb.org/t/p/w342/' + item.poster_path" alt="../assets/foto_assente.jpg">
+        <img v-if="item.poster_path != null" class="copertina" :src="'https://image.tmdb.org/t/p/w342/' + item.poster_path">
+        <img class="copertina" v-else src="../assets/foto_assente.jpg">
         <h2>{{ item.name }}</h2>
         <h3>Titolo originale: {{ item.original_name }}</h3>
         <h3>Lingua originale: {{ item.original_language }} <img class="bandiera" src="../assets/it.png" alt="Bandiera Italiana" v-if="item.original_language == 'it'"><img class="bandiera" src="../assets/en.png" alt="Bandiera Inglese" v-if="item.original_language == 'en'"></h3>
-        <h4>Voto degli utenti: <i v-for="i in 5" :key="i">{{ item.vote_average }}</i></h4>  
+        <h4>Voto degli utenti: {{ item.vote_average }} - {{ vote() }}</h4>  
+        <div>
+            <i 
+            v-for="n in vote()"
+            :key="`full-${n}`"
+            class="fas fa-star"
+            ></i>
+            <i
+            v-for="n in (5 - vote())"
+            :key="`empty-${n}`"
+            class="far fa-star"></i>
+        </div>
       </li>
     </div>
 </main>    
@@ -17,9 +29,11 @@ export default {
     name:"movies",
     props: ["item"],
 
-    // created: {
-    //   raiting: Math.floor(item.vote_average)
-    // }
+     methods: {
+      vote() {
+            return Math.ceil(this.item.vote_average/2);
+        }
+    }
 
 }
 </script>
